21edf62eacee81615c8f4aa4802e0d3da7169e4c
Harpreet Singh <harpreet@streammarket.co.uk>
2025-11-26 16:00:47 +0530

    push 2nd files

--- CreatePost.php
+++ CreatePost.php
@@ -12,15 +12,18 @@
 
 namespace StreamMarket\RoyalMailShipping\Controller\Adminhtml\Manifest;
 use Magento\Framework\HTTP\Client\Curl;
+use StreamMarket\RoyalMailShipping\Helper\Data as HelperData;
 /**
  * CreatePost Action
  */
 class CreatePost extends \Magento\Backend\App\Action
 {
+	
+	const XML_PATH_CLIENT_ID = 'carriers/smroyalmail/client_id';
+	
+    protected $helperData;
+	protected $scopeConfig;
 
-    /**
-     * @var \StreamMarket\RoyalMailShipping\Model\CarrierFactory
-     */
     private $carrierFactory;
 	
 	protected $curl;
@@ -28,39 +31,61 @@ class CreatePost extends \Magento\Backend\App\Action
     public function __construct(
 	\Magento\Backend\App\Action\Context $context,
     \StreamMarket\RoyalMailShipping\Model\CarrierFactory $carrierFactory,
-	Curl $curl
+	Curl $curl,
+	HelperData $helperData,
+	\Magento\Framework\App\Config\ScopeConfigInterface $scopeConfig
 			)
     {
         parent::__construct($context);
         $this->carrierFactory = $carrierFactory;
 		$this->curl = $curl;
+		$this->helperData = $helperData;
+		$this->scopeConfig = $scopeConfig;
     }
 
     public function execute()
     {
-		
-		
-        $URL = 'https://api.royalmail.net/shipping/v3/manifests/byservice';
-		  $jsonData = "[{\"ShipmentId\":\"$tracking_number\",\"ReasonForCancellation\":\"OrderCancelled\"}]";
-		 
-		  //set curl options
-		  $this->curl->setOption(CURLOPT_HEADER, 0);
-		  $this->curl->setOption(CURLOPT_TIMEOUT, 60);
-		  $this->curl->setOption(CURLOPT_RETURNTRANSFER, true);
-		  $this->curl->setOption(CURLOPT_CUSTOMREQUEST, "POST");
-		  
-		  //set curl header
-		  $this->curl->addHeader("Content-Type", "application/json");
-		  $this->curl->addHeader("accept", "application/json");
-		  $this->curl->addHeader("X-IBM-Client-Id", "d62ae68b-1797-4c84-a6d4-e80d87cf9993");
-		  $this->curl->addHeader("X-RMG-Auth-Token", "eyJraWQiOiJoczI1Ni1rZXkiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2MzUxNzE2NzIsImlhdCI6MTYzNTE1NzI3MiwidXNlcklkIjoiMDQxODI5MTAwMEFQSSIsInBhc3N3b3JkIjoiamxsMTBSeGE2UmlOSGdBUml1RWc3dE5OSHBNPSJ9.0aL7j8xUREtnBgCSZMS4ZnxlcKi6gs7VSoQSPeq9na0");
-		  //post request with url and data
-		  $this->curl->post($URL, $jsonData);
-		  //read response
-		  $response = $this->curl->getBody();
-		  $data = json_decode($response, TRUE);
-		  return $data;
-		
-    }
+		$token     = $this->helperData->generateToken();
+		$clientId  = $this->getSystemConfigValue(self::XML_PATH_CLIENT_ID);
 
+		$url = 'https://api.royalmail.net/shipping/v3/shipments/cancel';
+
+		// Payload (use json_encode instead of manual string building)
+		$payload = [
+			[
+				"ShipmentId"            => $tracking_number,
+				"ReasonForCancellation" => "OrderCancelled"
+			]
+		];
+
+		$jsonData = json_encode($payload);
+
+		// Set headers
+		$this->curl->addHeader("Content-Type", "application/json");
+		$this->curl->addHeader("Accept", "application/json");
+		$this->curl->addHeader("X-IBM-Client-Id", $clientId);
+		$this->curl->addHeader("X-RMG-Auth-Token", $token);
+
+		// Timeout and return behavior
+		$this->curl->setOption(CURLOPT_TIMEOUT, 60);
+		$this->curl->setOption(CURLOPT_RETURNTRANSFER, true);
+
+		// POST request
+		$this->curl->post($url, $jsonData);
+
+		// Response
+		$response = $this->curl->getBody();
+		$data = json_decode($response, true);
+
+		// Error handling
+		if (json_last_error() !== JSON_ERROR_NONE) {
+			$this->logger->error('Royal Mail API invalid JSON response: ' . $response);
+			return [
+				'error' => true,
+				'message' => 'Invalid JSON response received',
+				'raw' => $response
+			];
+		}
+		return $data;
+    }
 }
